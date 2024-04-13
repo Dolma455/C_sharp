a725d1458540fae00ec33221a27b241e67c32d2a
Dolma <dolma1918@gmail.com>
2024-04-13 17:10:59 +0545

    If else condition

--- crud_operation.cs
+++ crud_operation.cs
@@ -1,21 +1,111 @@
 // using System;
 // using System.Data;
-// using System.Data.MysqlClient;
-// namespace DatabaseTest {
-//     class Program {
-//         SqlConnection conn;
-//         SqlCommand cmd;
+// using MySql.Data.MySqlClient;
 
-//         void CreateConnection() {
-//             string constr= "Server=localhost,Database=CRUD_C_Sharp, user=root, password=root;
-//             conn=new MySqlConnection(cobstr);
+// namespace DatabaseTest
+// {
+//     class Program
+//     {
+//         MySqlConnection conn;
+//         MySqlCommand cmd;
+
+//         void CreateConnection()
+//         {
+//             string constr = "Server=localhost;Database=CRUD_C_Sharp;Uid=root;Pwd=root;";
+//             conn = new MySqlConnection(constr);
 //             conn.Open();
 //         }
 
-//         void InsertUdateDelete(string sql) {
-//             command=new MySqlCommand(sql,conn);
-//             command.ExecuteNonQuery();
-//             Console.WriteLine("Operation Performed Successfully")
+//         void InsertUpdateDelete(string sql, MySqlParameter[] parameters)
+//         {
+//             cmd = new MySqlCommand(sql, conn);
+//             cmd.Parameters.AddRange(parameters);
+//             cmd.ExecuteNonQuery();
+//             Console.WriteLine("Operation Performed Successfully");
+//         }
+
+
+//         void SelectRecords(string sql)
+//         {
+//             cmd = new MySqlCommand(sql, conn);
+//             MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+//             DataTable dt = new DataTable();
+//             adapter.Fill(dt);
+//             if (dt.Rows.Count != 0)
+//             {
+//                 Console.WriteLine("Sid\tName\tAddress");
+//                 for (int i = 0; i < dt.Rows.Count; i++)
+//                 {
+//                     string id = dt.Rows[i]["Sid"].ToString();
+//                     string name = dt.Rows[i]["name"].ToString();
+//                     string address = dt.Rows[i]["address"].ToString();
+//                     Console.WriteLine(id + "\t" + name + "\t" + address);
+//                 }
+//             }
+//             else
+//             {
+//                 Console.WriteLine("No records found.");
+//             }
+//         }
+
+//         static void Main(string[] args)
+//         {
+//             Program obj = new Program();
+//             try
+//             {
+//                 obj.CreateConnection();
+//             Start:
+//                 Console.WriteLine("1.Insert\t2.Update\t3.Delete\t4.Select");
+//                 Console.WriteLine("Enter your choice:");
+//                 int n = Convert.ToInt32(Console.ReadLine());
+//                 string sql = "", name = "", add = "";
+//                 int id = 0;
+//                 switch (n)
+//                 {
+//                     case 1:
+//                         Console.WriteLine("Enter Name of Student:");
+//                         name = Console.ReadLine();
+//                         Console.WriteLine("Enter Address of Student:");
+//                         add = Console.ReadLine();
+//                         sql = "INSERT INTO tblStudent(name,address) VALUES ('" + name + "','" + add + "')";
+//                         obj.InsertUpdateDelete(sql);
+//                         break;
+
+//                     case 2:
+//                         Console.WriteLine("Enter ID to be updated:");
+//                         id = Convert.ToInt32(Console.ReadLine());
+//                         Console.WriteLine("Enter name of student:");
+//                         name = Console.ReadLine();
+//                         Console.WriteLine("Enter address of Student:");
+//                         add = Console.ReadLine();
+//                         sql = "UPDATE tblStudent SET name='" + name + "', address='" + add + "' WHERE sid=" + id;
+//                         obj.InsertUpdateDelete(sql);
+//                         break;
+
+//                     case 3:
+//                         Console.WriteLine("Enter id to be deleted:");
+//                         id = Convert.ToInt32(Console.ReadLine());
+//                         sql = "DELETE FROM tblStudent WHERE sid=" + id;
+//                         obj.InsertUpdateDelete(sql);
+//                         break;
+
+//                     case 4:
+//                         sql = "SELECT * FROM tblStudent";
+//                         obj.SelectRecords(sql);
+//                         break;
+
+//                     default:
+//                         Console.WriteLine("Invalid Choice");
+//                         break;
+//                 }
+//                 goto Start;
+//             }
+//             catch (Exception e)
+//             {
+//                 Console.WriteLine(e.Message);
+//                 Console.WriteLine("Connection Failed !");
+//             }
+//             Console.ReadKey();
 //         }
 //     }
 // }
---
+++ if_else_condition.cs
@@ -0,0 +1,12 @@
+using System;
+class IfElse {
+    static void Main(string [] args) {
+        int num=10;
+        if (num%2==0) {
+            Console.WriteLine("Even Number");
+        }
+        else {
+            Console.WriteLine("Odd Number");
+        }
+    }
+}
